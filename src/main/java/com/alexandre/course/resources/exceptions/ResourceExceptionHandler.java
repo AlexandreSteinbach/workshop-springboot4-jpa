e990a4e54682b6eec24ae92c62d2f67b0a70d0bc
AlexandreSteinbach <alexandresteinbach2@gmail.com>
2026-02-11 11:57:06 -0300

    Exception handling - delete

--- src/main/java/com/alexandre/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/alexandre/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.alexandre.course.resources.exceptions;
 
+import com.alexandre.course.services.exceptions.DatabaseException;
 import com.alexandre.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -20,7 +21,11 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
-    public ResourceExceptionHandler() {
-        super();
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
     }
 }
